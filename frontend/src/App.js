2da5396a4291e867333f816dd6fa17e183a185c9
Niklas <niklasaavad@gmail.com>
2022-04-30 23:44:09 +0200

    Restructured sending messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { connect, sendMsg } from './api';
+import { connect } from './api';
 import Header from './components/Header/Header';
 import ChatHistory from './components/ChatHistory/ChatHistory';
 import ChatInput from './components/ChatInput/ChatInput';
@@ -19,18 +19,11 @@ const App = () => {
     });
   })
 
-  const send = (event) => {
-    if (event.key === "Enter") {
-      sendMsg(event.target.value);
-      event.target.value="";
-    }
-  }
-
   return (
     <div className='App'>
       <Header/>
       <ChatHistory chatHistory={chatHistory}/>
-      <ChatInput send={send}/>
+      <ChatInput/>
     </div>
   )
 };
--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./ChatInput.scss"
+import { sendMsg } from "../../api";
 
 const ChatInput = (props) => {
+    const send = (event) => {
+        if (event.key === "Enter") {
+          sendMsg(event.target.value);
+          event.target.value="";
+        }
+      }
+
     return(
         <div className="chat-input">
-            <input onKeyDown={props.send}/>
+            <input onKeyDown={send}/>
         </div>
     )
 }
