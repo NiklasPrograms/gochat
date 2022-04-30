df9ca2ca6c0429d22d605f20f143cee541e105fe
Niklas <niklasaavad@gmail.com>
2022-04-30 20:41:00 +0200

    For loop did only do one iteration

--- backend/pkg/websocket/pool.go
+++ backend/pkg/websocket/pool.go
@@ -19,33 +19,35 @@ func NewPool() *Pool {
 }
 
 func (pool *Pool) Start() {
-	select {
-	case client := <-pool.Register:
-		pool.Clients[client] = true
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
 
-		fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
 
-		for client := range pool.Clients {
-			fmt.Println(client)
-			client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
-		}
-		break
-	case client := <-pool.Unregister:
-		delete(pool.Clients, client)
+			for client := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			}
+			break
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
 
-		fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
 
-		for client := range pool.Clients {
-			fmt.Println(client)
-			client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
-		}
-		break
-	case message := <-pool.Broadcast:
-		fmt.Println("Sending message to all clients in Pool")
-		for client := range pool.Clients {
-			if err := client.Conn.WriteJSON(message); err != nil {
-				fmt.Println(err)
-				return
+			for client := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+			}
+			break
+		case message := <-pool.Broadcast:
+			fmt.Println("Sending message to all clients in Pool")
+			for client := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println(err)
+					return
+				}
 			}
 		}
 	}
